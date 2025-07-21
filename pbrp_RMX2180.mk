b2104cf8a898834d3432ea199cb55658bb0ff076
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-07-21 09:38:00 +0700

    Update pbrp_RMX2180.mk

--- pbrp_RMX2180.mk
+++ pbrp_RMX2180.mk
@@ -18,3 +18,17 @@ PRODUCT_NAME := pbrp_RMX2180
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2180
 PRODUCT_MANUFACTURER := realme
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
+
+# Bypass anti-rollback ROMs protection
+# Set build date to Jan 1 2009 00:00:00
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.date.utc=1230768000

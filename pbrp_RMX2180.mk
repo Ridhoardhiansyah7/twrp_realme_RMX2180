b1ad9d0a8f89e1e9791f45df0dcfddff5ccfd82b
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2025-07-21 09:36:42 +0700

    Create pbrp_RMX2180.mk

---
+++ pbrp_RMX2180.mk
@@ -0,0 +1,20 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit some common pbrp stuff.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
+
+# Inherit from RMX2180 device
+$(call inherit-product, device/realme/RMX2180/device.mk)
+
+PRODUCT_DEVICE := RMX2180
+PRODUCT_NAME := pbrp_RMX2180
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX2180
+PRODUCT_MANUFACTURER := realme
